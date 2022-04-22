62f3433b83c5ffbddab6e8f9ae2018274f74f0b3
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-04-21 20:35:28 -0500

    import { HomeComponent } from './pages/heroe/home.component';

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { HeroesRoutingModule } from './heroes-routing.module';
-import { ListComponent } from './pages/list/list.component';
-import { AddComponent } from './pages/add/add.component';
-import { EditComponent } from './pages/edit/edit.component';
-import { SearchComponent } from './pages/search/search.component';
-import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesRoutingModule } from "./heroes-routing.module";
 
+import { ListComponent } from "./pages/list/list.component";
+import { AddComponent } from "./pages/add/add.component";
+import { EditComponent } from "./pages/edit/edit.component";
+import { SearchComponent } from "./pages/search/search.component";
+import { HeroeComponent } from "./pages/heroe/heroe.component";
+import { HomeComponent } from "./pages/heroe/home.component";
 
 @NgModule({
   declarations: [
@@ -15,11 +16,9 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
     AddComponent,
     EditComponent,
     SearchComponent,
-    HeroeComponent
+    HeroeComponent,
+    HomeComponent,
   ],
-  imports: [
-    CommonModule,
-    HeroesRoutingModule
-  ]
+  imports: [CommonModule, HeroesRoutingModule],
 })
-export class HeroesModule { }
+export class HeroesModule {}

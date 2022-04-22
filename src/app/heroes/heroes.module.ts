13a3294d0fc16d9f8008b3e64cbdc1a8651791fe
David Espinoza <deerhou@gmail.com>
2022-04-22 14:49:03 -0500

    material install and home heroes sidebar

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { HeroesRoutingModule } from "./heroes-routing.module";
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HeroesRoutingModule } from './heroes-routing.module';
 
-import { ListComponent } from "./pages/list/list.component";
-import { AddComponent } from "./pages/add/add.component";
-import { SearchComponent } from "./pages/search/search.component";
-import { HeroeComponent } from "./pages/heroe/heroe.component";
-import { HomeComponent } from "./pages/home/home.component";
+import { ListComponent } from './pages/list/list.component';
+import { AddComponent } from './pages/add/add.component';
+import { SearchComponent } from './pages/search/search.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,6 @@ import { HomeComponent } from "./pages/home/home.component";
     HeroeComponent,
     HomeComponent,
   ],
-  imports: [CommonModule, HeroesRoutingModule],
+  imports: [CommonModule, FlexLayoutModule, HeroesRoutingModule, MaterialModule],
 })
 export class HeroesModule {}
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  //#region variables de sidenav
+  // @ViewChild('sidebar') sidebar: MatSidenav;
+  //#endregion variables de sidenav
   constructor() { }
 
   ngOnInit(): void {

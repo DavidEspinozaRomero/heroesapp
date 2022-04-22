0277a3c38b75746546cd6f1a95cd81c6b98e19d5
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-04-21 20:22:57 -0500

    heroes home component

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,23 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { ListComponent } from './pages/list/list.component';
-import { AddComponent } from './pages/add/add.component';
-import { EditComponent } from './pages/edit/edit.component';
-import { SearchComponent } from './pages/search/search.component';
-import { HeroeComponent } from './pages/heroe/heroe.component';
+import { ListComponent } from "./pages/list/list.component";
+import { AddComponent } from "./pages/add/add.component";
+import { EditComponent } from "./pages/edit/edit.component";
+import { SearchComponent } from "./pages/search/search.component";
+import { HeroeComponent } from "./pages/heroe/heroe.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 const routes: Routes = [
-  { path: 'listado', component: ListComponent },
-  { path: 'agregar', component: AddComponent },
-  { path: 'editar/:id', component: AddComponent },
-  { path: 'buscar', component: SearchComponent },
-  { path: ':id', component: HeroeComponent },
-  { path: '**', redirectTo: 'listado'  },
+  {
+    path: "",
+    component: HomeComponent,
+    children: [
+      { path: "listado", component: ListComponent },
+      { path: "agregar", component: AddComponent },
+      { path: "editar/:id", component: AddComponent },
+      { path: "buscar", component: SearchComponent },
+      { path: ":id", component: HeroeComponent },
+      { path: "**", redirectTo: "listado" },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HeroesRoutingModule { }
+export class HeroesRoutingModule {}

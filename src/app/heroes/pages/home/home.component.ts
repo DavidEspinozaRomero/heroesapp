edf97be545e657af0ffc2d8de96e64b36d54be77
David Espinoza <deerhou@gmail.com>
2022-04-22 16:01:58 -0500

    side nav spacer class

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -9,7 +9,16 @@ import { MatSidenav } from '@angular/material/sidenav';
 export class HomeComponent implements OnInit {
 
   //#region variables de sidenav
-  // @ViewChild('sidebar') sidebar: MatSidenav;
+  // @ViewChild('sidenav') sidenav: MatSidenav;
+  navItemList = [
+    // {title: 'login', routerLink: '/auth/login' },
+    // {title: 'registro', routerLink: '/auth/register' },
+    {icon: 'label', title: 'listado de Heroes', routerLink: './listado' },
+    {icon: 'add', title: 'agregar Heroe', routerLink: './agregar' },
+    {icon: 'search', title: 'buscar Heroe', routerLink: './buscar' },
+    {icon: 'label', title: 'editar', routerLink: './editar/1' },
+    {icon: 'label', title: 'heroe', routerLink: './12' },
+  ]
   //#endregion variables de sidenav
   constructor() { }
 

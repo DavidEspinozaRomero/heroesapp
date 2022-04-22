317bc9d13ef067b0c2902f30479e599e89c39d40
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-04-21 20:42:49 -0500

    edit deleted

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -5,7 +5,6 @@ import { HeroesRoutingModule } from "./heroes-routing.module";
 
 import { ListComponent } from "./pages/list/list.component";
 import { AddComponent } from "./pages/add/add.component";
-import { EditComponent } from "./pages/edit/edit.component";
 import { SearchComponent } from "./pages/search/search.component";
 import { HeroeComponent } from "./pages/heroe/heroe.component";
 import { HomeComponent } from "./pages/home/home.component";

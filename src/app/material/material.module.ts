c0a63935606755ad8b4525964cbd5e74e4419edd
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-04-23 08:21:40 -0500

    mat card

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 
-
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
-  exports: [],
+  exports: [MatCardModule],
 })
-export class MaterialModule { }
+export class MaterialModule {}

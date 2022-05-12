224760fb3bc5219c03b581c9a83d9a61bc4ae0b1
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-05-11 20:58:53 -0500

    dialog test

--- src/app/heroes/components/confirm/confirm.component.ts
+++ src/app/heroes/components/confirm/confirm.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { MatDialogRef } from "@angular/material/dialog";
 @Component({
-  selector: 'app-confirm',
-  templateUrl: './confirm.component.html',
-  styleUrls: ['./confirm.component.scss']
+  selector: "app-confirm",
+  templateUrl: "./confirm.component.html",
+  styleUrls: ["./confirm.component.scss"],
 })
 export class ConfirmComponent implements OnInit {
+  constructor(private dialogRef: MatDialogRef<ConfirmComponent>) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  borrar() {
+    this.dialogRef.close(true);
   }
 
+  cancelar() {
+    this.dialogRef.close();
+  }
 }

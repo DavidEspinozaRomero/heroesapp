01894288288003f3bf5a914691813b95975b3512
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-05-12 21:08:15 -0500

    sadf

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
+import { tap } from "rxjs/operators";
 import { environment } from "../../../environments/environment";
 import { Auth } from "../interfaces/auth.interface";
-import { tap } from "rxjs/operators";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
   //#region variables
-  private _auth: Auth | undefined;
+  _auth: Auth | undefined;
+  // private _auth: Auth | undefined;
 
-  public get auth(): Auth {
-    return { ...this._auth };
-  }
+  // get auth(): Auth {
+  //   return { ...this._auth };
+  // }
 
   //#endregion variables
   constructor(private http: HttpClient) {}

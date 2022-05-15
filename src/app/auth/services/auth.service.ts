39b8ed422ad31b4a0aef00c7d77d7e801871256d
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-05-14 20:28:24 -0500

    of

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
 import { tap, map } from "rxjs/operators";
 
 import { environment } from "../../../environments/environment";

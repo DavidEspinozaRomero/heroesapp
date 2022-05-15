aaef61ccb87577a713c6efbad0395b7bc8026257
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-05-14 20:25:36 -0500

    Observable

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
-import { Obserbable } from "rxjs";
+import { Observable } from "rxjs";
 import { tap, map } from "rxjs/operators";
+
 import { environment } from "../../../environments/environment";
 import { Auth } from "../interfaces/auth.interface";
 import { LocalStorageService } from "../../service/localStorage.service";
@@ -15,15 +16,16 @@ export class AuthService {
   get auth(): Auth {
     return { ...this._auth! };
   }
+  
 
   //#endregion variables
   constructor(private http: HttpClient, private _localStorage:LocalStorageService) {}
 
   verifyAuthentication(): Observable<boolean> {
-    if (!this._localStorageService.getStorage('user')) {
+    if (!this._localStorage.getStorage('user')) {
       return of(false)
     }
-    return this.http.get<Auth>(`${url}/usuarios/1`).pipe(
+    return this.http.get<Auth>(`${environment.urlApp}/usuarios/1`).pipe(
       map(auth => {
         console.log(auth);
         return true
@@ -34,8 +36,7 @@ export class AuthService {
   }
 
   login() {
-    const url = environment.urlApp;
-    return this.http.get<Auth>(`${url}/usuarios/1`).pipe(
+    return this.http.get<Auth>(`${environment.urlApp}/usuarios/1`).pipe(
       tap((auth) => (this._auth = auth)),
       tap((auth) => this._localStorage.setStorage("user", auth))
     );
